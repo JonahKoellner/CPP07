3f789e1c96dad5e622f9174e12bec99343701197
Jonah Köllner <koellnjo@gmail.com>
2024-03-05 16:31:40 +0100

    removed :: appending, unnecessery

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jonahkollner <jonahkollner@student.42.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 14:16:57 by jkollner          #+#    #+#             */
-/*   Updated: 2024/02/27 17:11:06 by jonahkollne      ###   ########.fr       */
+/*   Updated: 2024/03/05 16:31:16 by jonahkollne      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@
 int main( void ) {
 	int a = 2;
 	int b = 3;
-	::swap( a, b );
+	swap( a, b );
 	std::cout << "a = " << a << ", b = " << b << std::endl;
 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 	std::string c = "chaine1";
 	std::string d = "chaine2";
-	::swap(c, d);
+	swap(c, d);
 	std::cout << "c = " << c << ", d = " << d << std::endl;
 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
